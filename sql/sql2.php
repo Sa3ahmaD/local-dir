9a7ea51615e7e475c85af6185c80e1e4279dac71
saleh <salehahmadbabu@gmail.com>
2021-03-01 13:27:19 +0600

    adding data through php prepare statement

--- sql/sql2.php
+++ sql/sql2.php
@@ -5,4 +5,21 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'students');
 
-$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$connection = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$con_prepare = $connection->prepare("INSERT INTO newstudents(name,gender,roll,class,section) VALUES(?,?,?,?,?);");
+$con_prepare->bind_param('ssiis', $name, $gender, $roll, $class, $section);
+
+$data = array_map('str_getcsv', file('./babynames-clean.csv'));
+shuffle($data);
+$_students = [];
+$sections = ['A','B','C','D'];
+
+for($i = 0; $i < 1000; $i++) {
+  $name = $data[$i][0];
+  $gender = $data[$i][1];
+  $section = $sections[array_rand($sections)];
+  $class = rand(1,10);
+  $_students["{$class}{$section}"][] = 1;
+  $roll = count($_students["{$class}{$section}"]);
+  $con_prepare->execute();
+}
