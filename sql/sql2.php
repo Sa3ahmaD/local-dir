5b4b6e35d5ee0adc944985245d0db6efa85e3ab5
saleh <salehahmadbabu@gmail.com>
2021-03-01 12:56:35 +0600

    updating roll number to students table solved.

--- sql/sql2.php
+++ sql/sql2.php
@@ -9,17 +9,25 @@ $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $query = "SELECT id, section, class FROM students";
 $result = mysqli_query($connection, $query);
 $_students = [];
+// print_r($row);
 
+// $roll = '';
+// $class = '';
+// $section = '';
 $queries = '';
 if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_assoc($result)) {
+    // print_r($row);
     $id = $row['id'];
     $class = $row['class'];
     $section = $row['section'];
     $_students["{$class}{$section}"][] = 1;
     $roll = count($_students["{$class}{$section}"]);
-    $queries = "UPDATE `students` SET `roll`={$roll} WHERE id={$id};";
+    // printf("%d:%s:%d<br>",$row['class'],$row['section'],$roll);
+    $queries .= "UPDATE `students` SET `roll`={$roll} WHERE id={$id};";
+    // print_r($queries);
     echo "<br>";
     mysqli_multi_query($connection, $queries);
+    // $updateRollSt->execute();
   }
 }
